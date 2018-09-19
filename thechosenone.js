5c5e01cbeae4603bdc8939f0d0ada4509e3a23ae
Joshua Cox <mongst@gmail.com>
2018-09-18 20:07:17 -0400

    Update - added text to index.html

--- thechosenone.js
+++ thechosenone.js
@@ -3,7 +3,7 @@
 // Someone should write the java for this page to auto-generate its own 'imgs' string from the files found in the folder.
 
 
-var imgs = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']    //  creates variable string with a list of the images
+var imgs = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg', '11.jpg']    //  creates variable string with a list of the images
     i = Math.floor(Math.random()*imgs.length);    // creates random number based on number of items in string
 
 
